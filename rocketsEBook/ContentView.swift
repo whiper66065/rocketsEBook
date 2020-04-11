e7c84ce5fca4baf4477d5361a359a97d41883345
yujenpan <whiper66065@gmail.com>
2020-04-11 20:31:40 +0800

    Initial Commit

---
+++ rocketsEBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rocketsEBook
+//
+//  Created by 潘昱任 on 2020/4/11.
+//  Copyright © 2020 ntouyujen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
